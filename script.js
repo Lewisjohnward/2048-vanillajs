2a628923cb91a5f5e79dd2093bb2373c69d94f89
@Lewisjohnward <lewisjohnward+git1@gmail.com>
2023-10-26 19:57:04 +0100

    working on logic

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const WIDTH = 5
 const HEIGHT = 5
+const DELAY = 500
 
 const container = document.querySelector(".container")
 
@@ -13,7 +14,7 @@ for (let i = 0; i < grid.length; i++){
     for (let i = 0; i < grid[0].length; i++){
         const element = document.createElement("div")            
         element.className = "grid"
-        element.innerText = "1"
+        if(i == 0)element.innerText = "1"
         rowContainer.append(element)
     }
     container.append(rowContainer)
@@ -32,9 +33,47 @@ for (let i = 0; i < grid.length; i++){
 //    container.append(rowContainer)
 //}
 
+const generateStartingPositions = () => {
+    const grid = [[],[],[]]
+    const nodes = document.querySelectorAll(".grid")
+    nodes.forEach((d, i) => {
+        grid[i] = d.innerText
+    })
+    return grid
+}
 
-const handleKeyboardEvent = (e) => {
-    console.log(e.key)
+const nextFrame = (grid) => {
+    //move everything right by 1
+    let previous
+    const updatedGrid = grid.map((d) => {
+        if (previous == undefined) {
+            previous = d
+            return ""
+        }
+
+        return previous
+    })
+    console.log(updatedGrid)
+
+}
+
+const handleKeyboardEvent = async (e) => {
+    if (e.key == "l") {
+        const grid = generateStartingPositions()
+        for(let i = 0; i < 1; i++){
+            nextFrame(grid)
+        }
+        //const grid = document.querySelectorAll(".grid")
+        await new Promise(resolve => setTimeout(resolve, DELAY))
+        grid[0].innerText = ""
+        grid[1].innerText = "1"
+        await new Promise(resolve => setTimeout(resolve, DELAY))
+        grid[1].innerText = ""
+        grid[2].innerText = "1"
+        await new Promise(resolve => setTimeout(resolve, DELAY))
+
+
+    }
 }
 
 document.body.addEventListener("keydown", handleKeyboardEvent)
